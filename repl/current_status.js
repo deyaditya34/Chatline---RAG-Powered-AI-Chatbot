a7ac2e060f9b440b436bb5d8d4171a35ed0abdb7
Aditya Dey <deyaditya34@gmail.com>
2026-05-04 01:48:20 +0530

    updated the variables

--- repl/current_status.js
+++ repl/current_status.js
@@ -1,4 +1,4 @@
-import {ai_model, system_instruction} from "../ai_model.js";
+import {ai_model, system_instruction_message} from "../ai_model.js";
 import {mode} from "../command.js";
 
 export function print_current_status() {
@@ -9,6 +9,6 @@ export function print_current_status() {
 	process.stdout.write("\x1B[38;2;160;160;220m");
 	console.log(`\tModel\t\t\t\t${ai_model}`);
 	console.log(`\tMode\t\t\t\t${mode}`);
-	console.log(`\tSystem Instruction\t\t${system_instruction}`);
+	console.log(`\tSystem Instruction\t\t${system_instruction_message}`);
 	process.stdout.write("\x1B[0m");	
 }
