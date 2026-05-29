7995b0f0bbf8007614e66267a9502b35a548e7cf
Aditya Dey <deyaditya34@gmail.com>
2026-05-29 18:10:57 +0530

    completed the delete feature by deleting vectors with the chat

--- repl/delete_conversation.js
+++ repl/delete_conversation.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { read_user_input } from "../readline.js";
 import user_prompts from "../prompts/default_user_prompts.json" with {type: "json"};
+import { delete_document } from "../database.js";
 
 export async function delete_conversation(conv_id) {
 	if (!conv_id || !Number(conv_id)) {
@@ -8,12 +9,15 @@ export async function delete_conversation(conv_id) {
 		return;
 	}
 
-	const chat_save_dir =
-		`${process.env.CONV_STORAGE_DIR}/${process.env.STATELESS_CONV_STORAGE_DIR}`; 
+	const chat_save_dir_user =
+		`${process.env.CONV_STORAGE_DIR}/${process.env.STATELESS_CONV_STORAGE_DIR}`;
 
 	const conversation_list = await fs.readdirSync(
-		`${chat_save_dir}`
+		`${chat_save_dir_user}`
 	);
+
+	const chat_save_dir_model =
+		`${process.env.CONV_STORAGE_DIR}/${process.env.STATELESS_CONV_TOKEN_BASED_SLIDING_DIR}`;
 
 	if (Number(conv_id) > conversation_list.length) {
 		console.log("invalid user input");
@@ -21,10 +25,26 @@ export async function delete_conversation(conv_id) {
 	}
 
 	const fileName = `${conversation_list[Number(conv_id) - 1]}`;
-	const filePath = `${chat_save_dir}/${fileName}`;
+	const filePath_user = `${chat_save_dir_user}/${fileName}`;
+	const filePath_model = `${chat_save_dir_model}/${fileName}`;
+
+	const file_content = fs.readFileSync(filePath_user);
+
+	const delete_criteria = {
+		must: [
+			{
+				key: "conversation_id",
+				match: {
+					value: fileName
+				}
+			}
+		]
+	}
+	await delete_document(delete_criteria)
 
 	try {
-		await fs.unlinkSync(filePath);
+		await fs.unlinkSync(filePath_user);
+		await fs.unlinkSync(filePath_model);
 		console.log(`conversation - '${fileName}' deleted`);
 	} catch (err) {
 		console.log("err in deleting file -", err);
